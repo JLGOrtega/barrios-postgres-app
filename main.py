b8f96275cd121e4847b8f500ff3194e102a35a92
JLGOrtega <101733471+JLGOrtega@users.noreply.github.com>
2023-03-10 20:44:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-from flask import Flask, jsonify
 import os
 
+from flask import Flask, request, jsonify
+from sqlalchemy import create_engine
+import pandas as pd
+
 app = Flask(__name__)
+@app.route("/barrios/all", methods=["GET"])
+def get_all_barrios():
+    # Crea un motor de conexión a la base de datos PostgreSQL
+    engine = create_engine('postgresql://postgres:5qViCt4PBn5ISzNTe3nj@containers-us-west-121.railway.app:7703/railway')
+    datos = pd.read_sql('select * from "Barrios"', con=engine)
+    return jsonify(datos.to_json())
 
+@app.route("/barrios/by_name", methods=["GET"])
+def get_barrio_byname():
+    barrio = request.args["name"]
+    # Crea un motor de conexión a la base de datos PostgreSQL
+    engine = create_engine('postgresql://postgres:5qViCt4PBn5ISzNTe3nj@containers-us-west-121.railway.app:7703/railway')
+    datos = pd.read_sql(f"""select * from "Barrios" where nombre = '{barrio}'""", con=engine)
+    return jsonify(datos.to_json())
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route("/barrios/by_area", methods=["GET"])
+def get_barrio_byarea():
+    area_min = request.args["area_min"]
+    area_max = request.args["area_max"]
+    # Crea un motor de conexión a la base de datos PostgreSQL
+    engine = create_engine('postgresql://postgres:5qViCt4PBn5ISzNTe3nj@containers-us-west-121.railway.app:7703/railway')
+    datos = pd.read_sql(f"""select * from "Barrios" 
+                            where gis_gis_barrios_area > {area_min} 
+                            and gis_gis_barrios_area < {area_max} """, con=engine)
+    return jsonify(datos.to_json())
 
 
 if __name__ == '__main__':
